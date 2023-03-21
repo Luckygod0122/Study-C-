d254fe4b6ce82516121e77d08ea97fa3052edd40
luckybot7 <109932028+Luckygod0122@users.noreply.github.com>
2023-03-22 02:54:43 +0900

    2023.03.22
    인라인함수, 이름공간, 매크로 함수 공부
    함수 오버로딩 복습

---
+++ C++ 1-3/C++ 1-3/C++ 1-3.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+
+using namespace std;
+template <typename T> // template << 자료형에 의존하지 않음	
+inline T SQUARE(T x) //함수의 몸체부분이 함수의 호출문장을 완전히 대체함
+{
+	return x * x;
+
+}
+namespace BestComImp1 //namespace 라는 이름공간
+{
+	void SimpleFunc(void);
+	
+}
+namespace BestComImp1
+{
+	void PrettyFunc(void);
+	
+}
+namespace ProgComImp1
+{
+	void SimpleFunc(void);
+}
+int main()
+{
+	BestComImp1::SimpleFunc();
+	cout << SQUARE(5) << endl;
+	cout << SQUARE(12) << endl;
+	return 0;
+}
+
+void BestComImp1::SimpleFunc(void)
+{
+	cout << "BestCom이 정의한 함수" << endl;
+	PrettyFunc(); // 동일 이름공간
+	ProgComImp1::SimpleFunc(); //다른 이름공간
+}
+void BestComImp1::PrettyFunc(void)
+{
+	cout << "So Pretty!!" << endl;
+}
+void ProgComImp1::SimpleFunc(void)
+{
+	cout << "ProgCom이 정의한 함수" << endl;
+}
---
+++ C++1-2/C++1-2/C++1-2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+using namespace std;
+// C++ 1-2 함수의 오버로딩: 함수의 이름은 같지만 매개변수의 종류나 개수가 다름
+// 반환형은 함수 호출시 함수를 구분하는 기준이 될 수 없다.
+void swap(int* a, int* b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+void swap(char* a, char* b)
+{
+	char temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+void swap(double* a, double* b)
+{
+	double temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int Adder(int num1 = 1, int num2 = 2)
+{
+	return num1 + num2;
+}
+int BoxVolume(int length, int width, int height)
+{
+	return length * width * height;
+}
+int BoxVolume(int length, int width)
+{
+	return length * width * 1;
+}
+int BoxVolume(int length)
+{
+	return length * 1 * 1;
+}
+int BoxVolume()
+{
+	return 1;
+}
+int main(void)
+{
+
+	int num1 = 20, num2 = 30;
+	swap(&num1, &num2);
+	cout << num1 << ' ' << num2 << endl;
+
+	char ch1 = 'A', ch2 = 'Z';
+	swap(&ch1, &ch2);
+	cout << ch1 << ' ' << ch2 << endl;
+
+	double dbl1 = 1.111, dbl2 = 5.555;
+	swap(&dbl1, &dbl2);
+	cout << dbl1 << ' ' << dbl2 << endl;
+	cout << Adder() << endl;
+
+	cout << Adder(5) << endl;
+	cout << Adder(3, 5) << endl;
+
+	cout << "[3,3,3] : " << BoxVolume(3, 3, 3) << endl;
+	cout << "[5,5,D] : " << BoxVolume(5, 5) << endl;
+	cout << "[7,D,D] : " << BoxVolume(7) << endl;
+	cout << "[D,D,D] : " << BoxVolume() << endl;
+
+	return 0;
+}
+
+
